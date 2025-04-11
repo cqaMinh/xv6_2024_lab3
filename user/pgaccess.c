f167f18fcdb346aadb5a4cf88e909029ce7e5d02
cqaMinh <146867398+cqaMinh@users.noreply.github.com>
2025-04-11 20:53:22 +0700

    Update pgaccess.c

--- user/pgaccess.c
+++ user/pgaccess.c
@@ -3,11 +3,5 @@
 #include "kernel/stat.h"
 int pgaccess(void *start, int n, unsigned int *bitmap)
 {
-    for (int i = 0; i < n; i++) {
-        if (/* điều kiện kiểm tra access */) {
-            bitmap[i / 64] |= (1ULL << (i % 64));  // Sử dụng uint64 * để ghi kết quả
-        }
-    }
-    
-    return 0;  // Thành công
+    return syscall(SYS_pgaccess, start, n, bitmap);
 }
